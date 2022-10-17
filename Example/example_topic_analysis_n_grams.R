098cdcb7753df7cde692c494a2edde82776ef096
Josiah McMillan <josiah.mcmillan2@gmail.com>
2022-10-17 16:13:40 +0200

    beginning a new example

---
+++ Example/example_topic_analysis_n_grams.R
@@ -0,0 +1,32 @@
+# Author: Josiah McMillan
+# Date 2022-10-17
+# PresDebate Data Example: Topic analysis and n-grams
+
+
+# Debate transcript data
+
+# library(devtools) # -- uncomment for install
+# install_github("josiahmcmillan/presdebates") # -- uncomment for install
+library(PresDebates)
+
+# Text cleaning packages
+library(tidytext)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(textdata)
+library(textclean)
+library(quanteda)
+
+# Plotting 
+library(scales)
+library(forcats)
+library(ggplot2)
+
+# Scraping webpages
+library(rvest)
+
+# Topic Model
+library(topicmodels)
+
+
